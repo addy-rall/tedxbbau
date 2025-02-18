0db8df865f09f868fbad882450da60d93fca4885
jay-kishan010 <jaykishan5691@gmail.com>
2025-02-18 10:53:42 +0530

    home page and navbar

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="text-center p-5 bg-black text-white">
+        <p>&copy; TEDxBBAU. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
