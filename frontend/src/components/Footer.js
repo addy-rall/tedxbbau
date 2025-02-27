5ebb20d31429cdcabc4fad8ab42e37ff76b026d0
Addya Mishra <amishra9336@gmail.com>
2025-02-27 23:34:55 +0530

    Delete frontend directory

--- frontend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="text-center p-5 bg-black text-white">
-        <p>&copy; TEDxBBAU. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
