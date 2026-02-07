2edc708a78e82f25058c5f173a8f83577b7de781
Adya <amishra9336@gmail.com>
2026-02-07 23:23:56 +0530

    Update App.jsx

--- tedxbbau/tedxbbau/src/App.jsx
+++ tedxbbau/tedxbbau/src/App.jsx
@@ -30,9 +30,6 @@ function App() {
             <Line />
             <ChiefSection/>
             <SpeakerSection />
-            <Line />
-            <Banner />
-            <Line />
             <Footer />
           </div>
         }
